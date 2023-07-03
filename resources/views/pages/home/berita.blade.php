13aef5f05949df4ce4abb090f102fd329836db90
BimaaTio <bimaatio10@gmail.com>
2023-07-03 14:14:30 +0700

    Update 3/07/2023

--- resources/views/pages/home/berita.blade.php
+++ resources/views/pages/home/berita.blade.php
@@ -53,9 +53,9 @@
         </div>
         @endforeach
       </div>
-      <div class="w-full lg:w-1/3 mt-8 sm:w-full px-4">
+      <div class="w-full lg:w-1/3 mt-8 px-4">
         <h1 class="text-lg font-medium text-center dark:text-white mb-2">Berita Terakhir</h1>
-        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div class="max-w p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <ul class="space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
             @foreach ($berita as $bl)
             <li class="my-4">
